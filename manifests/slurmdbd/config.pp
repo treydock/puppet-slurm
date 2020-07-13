f15b9fad5aed9e65af6984fcf111c9d134d4a485
Trey Dockendorf <tdockendorf@osc.edu>
2020-07-13 16:22:57 -0400

    DIsable diff of slurmdbd.conf

--- manifests/slurmdbd/config.pp
+++ manifests/slurmdbd/config.pp
@@ -27,12 +27,13 @@ class slurm::slurmdbd::config {
   }
 
   file { 'slurmdbd.conf':
-    ensure  => 'file',
-    path    => $slurm::slurmdbd_conf_path,
-    owner   => $slurm::slurm_user,
-    group   => $slurm::slurm_user_group,
-    mode    => '0600',
-    content => template('slurm/slurmdbd/slurmdbd.conf.erb'),
-    notify  => $slurm::slurmdbd_notify,
+    ensure    => 'file',
+    path      => $slurm::slurmdbd_conf_path,
+    owner     => $slurm::slurm_user,
+    group     => $slurm::slurm_user_group,
+    mode      => '0600',
+    content   => template('slurm/slurmdbd/slurmdbd.conf.erb'),
+    notify    => $slurm::slurmdbd_notify,
+    show_diff => false,
   }
 }
