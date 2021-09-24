69bb9d4278d73c4939149e4b9b9143372d22d748
Trey Dockendorf <tdockendorf@osc.edu>
2021-09-24 15:46:56 -0400

    Set idempotent charset for Ubuntu 20.04

--- manifests/slurmdbd/db.pp
+++ manifests/slurmdbd/db.pp
@@ -1,6 +1,11 @@
 # @api private
 class slurm::slurmdbd::db {
   if $slurm::manage_database {
+    if $facts['os']['release']['major'] == '20.04' {
+      $charset = 'utf8mb3'
+    } else {
+      $charset = 'utf8'
+    }
     if $slurm::export_database {
       @@mysql::db { "slurmdbd_${::fqdn}":
         user     => $slurm::slurmdbd_storage_user,
@@ -8,6 +13,7 @@ class slurm::slurmdbd::db {
         dbname   => $slurm::slurmdbd_storage_loc,
         host     => $::fqdn,
         grant    => ['ALL'],
+        charset  => $charset,
         tag      => $slurm::export_database_tag,
       }
     } else {
@@ -16,6 +22,7 @@ class slurm::slurmdbd::db {
         password => $slurm::slurmdbd_storage_pass,
         host     => $slurm::slurmdbd_storage_host,
         grant    => ['ALL'],
+        charset  => $charset,
       }
     }
   }
