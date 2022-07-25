4b3fe7a367abc7bbaf715478b18d76aa87646914
Trey Dockendorf <tdockendorf@osc.edu>
2022-07-25 08:30:19 -0400

    Update README and example

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -7,10 +7,16 @@
 # @param config_name
 #   Name of configuration file
 #
-# @example Create /etc/slurm/slurm-pitzer.conf
-#   slurm::conf { 'pitzer':
-#     configs => ...
-#   }
+# @example Create /etc/slurm/slurm-ascend.conf
+#
+#  include slurm
+#  $cluster_conf = {
+#    'ClusterName'   => 'ascend',
+#    'SlurmctldHost' => 'ascend-slurm01.example.com',
+#  }
+#  slurm::conf { 'ascend':
+#    configs => $slurm::slurm_conf + $cluster_conf,
+#  }
 #
 define slurm::conf (
   Hash $configs = {},
