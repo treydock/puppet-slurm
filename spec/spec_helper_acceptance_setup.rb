8e6996348566447b7c005aeab2974c15d60e8f1a
Trey Dockendorf <tdockendorf@osc.edu>
2024-11-27 12:23:44 -0500

    Support Slurm 24.05.x

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -11,7 +11,7 @@ RSpec.configure do |c|
   c.slurm_repo_baseurl = ENV['SLURM_BEAKER_repo_baseurl'] || nil
 
   c.add_setting :slurm_version
-  c.slurm_version = ENV['SLURM_BEAKER_version'] || '23.11.5'
+  c.slurm_version = ENV['BEAKER_slurm_version'] || '23.11.5'
 
   if ENV['BEAKER_set'] =~ %r{cluster}
     slurmctld_host = 'slurmctld'
